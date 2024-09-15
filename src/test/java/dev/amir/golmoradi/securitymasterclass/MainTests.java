fd1e92a01c48a2722cd2c97c9d3cb31a125f103b
Amir <LeoQueenzer2.0@gmail.com>
2024-09-16 01:05:55 +0330

    dummyTest

--- src/test/java/dev/amir/golmoradi/securitymasterclass/MainTests.java
+++ src/test/java/dev/amir/golmoradi/securitymasterclass/MainTests.java
@@ -1,13 +1,19 @@
 package dev.amir.golmoradi.securitymasterclass;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class MainTests {
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Test
     void contextLoads() {
+        System.out.println("--- Encoded Password ---" + passwordEncoder.encode("password"));
     }
 
 }
